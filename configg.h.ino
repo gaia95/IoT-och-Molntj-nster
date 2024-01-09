fe6f95f5f34972faaeae197cf8c59e70a84069d7
Meriem <114213504+gaia95@users.noreply.github.com>
2024-01-09 15:36:40 +0100

    Add files via upload

---
+++ configg.h.ino
@@ -0,0 +1,14 @@
+// Copyright (c) Microsoft Corporation. All rights reserved.
+// SPDX-License-Identifier: MIT
+
+// Wifi
+#define IOT_CONFIG_WIFI_SSID "SSID"
+#define IOT_CONFIG_WIFI_PASSWORD "PWD"
+
+// Azure IoT
+#define IOT_CONFIG_IOTHUB_FQDN "Your Azure IoT host name.azure-devices.net"
+#define IOT_CONFIG_DEVICE_ID "DEVICE ID"
+#define IOT_CONFIG_DEVICE_KEY "DEVICE KEY"
+
+// Publish 1 message every 2 seconds
+#define TELEMETRY_FREQUENCY_MILLISECS 2000
